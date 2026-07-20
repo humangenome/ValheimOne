b2a61085d693c266012d8f31e05d0006c372557e
HumanGenome <humangenome@users.noreply.github.com>
2026-07-19 19:26:43 -0700

    Version 0.7.1

--- src/ValheimOne/Networking/VersionInfo.cs
+++ src/ValheimOne/Networking/VersionInfo.cs
@@ -4,7 +4,7 @@ namespace ValheimOne.Networking;
 
 public static class VersionInfo
 {
-    public const string PluginVersion = "0.7.0";
+    public const string PluginVersion = "0.7.1";
     public const string SupportedGameVersion = "0.221.12";
 
     // Bump monotonically whenever the network payload or synced-config contract changes incompatibly.
